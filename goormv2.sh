2a5fcd47831438714cec54c4e8bc79fe1b9c1e07
mmpuq <44998325+mmpuq@users.noreply.github.com>
2020-07-25 10:55:33 +0800

    Update goormv2.sh

--- goormv2.sh
+++ goormv2.sh
@@ -13,11 +13,11 @@ rm -f /v2ray/v2ctl
 wget --no-check-certificate https://github.com/byxiaopeng/goorm-v2ray/raw/master/v2ctl
 chmod +x /v2ray/v2ctl
 rm -f /v2ray/config.json
-wget --no-check-certificate https://github.com/byxiaopeng/goorm-v2ray/raw/master/config.json
+wget --no-check-certificate https://github.com/eshion/goorm-v2ray/blob/master/config.json
 rm -f /v2ray/1.key
-wget --no-check-certificate https://github.com/byxiaopeng/goorm-v2ray/raw/master/1.key
+wget --no-check-certificate https://github.com/eshion/goorm-v2ray/blob/master/1.key
 rm -f /v2ray/1.crt
-wget --no-check-certificate https://github.com/byxiaopeng/goorm-v2ray/raw/master/1.crt
+wget --no-check-certificate https://github.com/eshion/goorm-v2ray/blob/master/1.crt
 
 #修改系统为北京时间
 cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
